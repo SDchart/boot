0161b23aec549809030e260dba88728853c6e8ec
SDchart <369999245@qq.com>
2019-06-20 14:05:30 +0800

    c

--- ex/js1.js
+++ ex/js1.js
@@ -1,16 +1,19 @@
 function compress(x){
-    var attr=[];
+    // var attr=[];
     var father=[];
-    for(var key in x[0]){
-        attr.push(key);
-    };
+    // for(var key in x[0]){
+    //     attr.push(key);
+    // };
+    var attr=Object.keys(x[0]);
     father.push(attr);
     for(var item of x){
-        var son=[];
-        for(var i in item){
-            son.push(item[i]);
-        }
+    //     var son=[];
+    //     for(var i in item){
+    //         son.push(item[i]);
+    //     }
+        var son=Object.values(item);
         father.push(son);
+    // console.log(son);
     }
     return father;
 }
@@ -18,5 +21,6 @@ function compress(x){
 
 var arr=[{name:'zhangsan',age:20,sex:'male'},{name:'lisi',age:19,sex:'female'},{name:'wangwu',age:23,sex:'male'}];
 
+// console.log(Object.values(arr),Object.keys(arr));
 
 console.log(compress(arr));
